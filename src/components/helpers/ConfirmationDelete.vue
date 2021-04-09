02476d8b4a8267cc4486b8d6d5bc5a057e45f444
NeoZis <Andreyrep17@gmail.com>
2021-04-09 17:40:01 +0300

    [Add] - confirmation delete modal window

---
+++ src/components/helpers/ConfirmationDelete.vue
@@ -0,0 +1,82 @@
+<template>
+  <modal-window
+      cancelButton
+      withoutButton
+      deleteButton
+      @close="$emit('input', false)"
+      :value="value">
+    <template #title>
+      Confirm deletion
+    </template>
+    <template #default>
+      <div class="main-block">
+        <p>If you want to delete the item, click the button “Delete”, or “Cancel” to cancel</p>
+      </div>
+    </template>
+    <template
+        #button>Save changes</template>
+  </modal-window>
+</template>
+
+<script>
+import ModalWindow from "@/components/ModalWindow";
+
+export default {
+  name: "ConfirmationDelete",
+  components: { ModalWindow},
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../style/variables";
+
+.main-block{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  @include fontPoppins(12px, 400, 20px);
+  .image-block{
+    display: flex;
+    margin-bottom: 30px;
+    justify-content: center;
+  }
+  .image{
+    width: 150px;
+    height: 100px;
+    border: 1px dashed #E7E6E6;
+    border-radius: 10px;
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2));
+  }
+  .image-cropper {
+    width: 150px;
+    height: 100px;
+    //background: yellow;
+    border: 1px dashed #E7E6E6;
+    border-radius: 10px;
+    cursor: pointer;
+    font-weight: 500;
+    color: #2F3741;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 20px;
+  }
+  .info {
+    display: flex;
+    flex-direction: column;
+    &-row {
+      margin-top: 10px;
+      display: flex;
+      &.descriptions{
+        //justify-content: space-between;
+      }
+    }
+  }
+}
+</style>
