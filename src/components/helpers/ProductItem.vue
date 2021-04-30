aaaeb1976374e1ab41d23dbffbfec0ae7d8593a8
Andrey Porodnov <andreyrep17@gmail.com>
2021-04-30 10:01:57 +0300

    [Fix] - promotiomal price for shop item

--- src/components/helpers/ProductItem.vue
+++ src/components/helpers/ProductItem.vue
@@ -3,7 +3,7 @@
     <button class="item-edit" @click="$emit('open-modal')"></button>
     <img :src="item.imageUri ? item.imageUri : require('../../assets/bottle.svg')" alt="">
     <div class="item-info">
-      <div class="item-info-row"><span class="price">${{item.price}}</span><span class="price-promo">$27.00</span></div>
+      <div class="item-info-row"><span class="price">${{getPromotionalPrice}}</span><span v-show="getOldPrice" class="price-promo">${{ getOldPrice }}</span></div>
       <div class="item-info-row"><span class="volume">{{item.volume}} LT</span></div>
       <div class="item-info-row">
         <p class="description">
@@ -20,6 +20,16 @@ export default {
   props: {
     item: {
       type: Object
+    }
+  },
+  computed: {
+    getPromotionalPrice () {
+      if(this.item.sale) {
+        return (this.item.price * (1-this.item.sale.percent*0.01))
+      } else return this.item.price
+    },
+    getOldPrice () {
+      return this.item.sale ? Number(this.item.price).toFixed(2) : 0
     }
   }
 }
